41d06d1598c4fadc9b3a0b3609371097af871d37
zoey03yang <zxcsuki@gmail.com>
2023-07-12 18:01:16 +0800

    新增留言板

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -65,6 +65,56 @@
               </div>
             </div>
           </section>
+          <section class="product_message">
+            <div class="message_title">
+              <div class="block"></div>
+              <div>{{ productMessage.Title }}</div>
+            </div>
+            <div class="message_area">
+              <div class="message_all" v-for="item in messageArea" :key="item">
+                <div class="user_pic">
+                  <img class="mark" src="~@/assets/images/products/mark.png"
+                    alt="mark"/> 
+                  <img class="user" src="~@/assets/images/products/message_pic01.png"
+                  alt="message_pic01"/>  
+                </div>
+                <div class="message_text">
+                  <div class="message_name">{{ item.name }}</div>
+                  <div class="message_text_all">{{ item.textAll }}</div>
+                  <div class="message_date">
+                    <img
+                    src="~@/assets/images/products/edit.png"
+                    alt="edit"
+                    />
+                    {{ item.date }}</div>
+                </div>
+              </div>
+            </div>
+            <div class="message_importing">
+              <div class="user_pic">
+                  <img
+                  src="~@/assets/images/products/importing_pic.png"
+                  alt="importing_pic"
+                />
+              </div>
+              <div class="importing_text">
+                <textarea placeholder="請輸入留言內容......" name="comment" form="usrform"></textarea>
+                <input type="submit" value="送出" />
+              </div>
+            </div>
+            <div class="notice">
+              <div class="notice_icon">
+                <img
+                  src="~@/assets/images/products/notice.png"
+                  alt="notice"
+                />
+              </div>
+              <div class="notice_text">
+                <div class="title">{{ noticeText.Title }}</div>
+                <div class="text">{{ noticeText.Text }}</div>
+              </div>
+            </div>
+          </section>
         </main>
       </section>
     </div>
@@ -104,6 +154,21 @@ export default {
         { title: "Email : ", text: "hawhaw052@gmail.com" },
         { title: "Phone : ", text: "0953299481" },
       ],
+      productMessage:{
+        Title:"留言板" ,
+      },
+      messageArea: [
+        {name:"棒球專家小楊",
+         textAll:"小弟這隻是Louisville slugger木棒，打了大約四年，有些地方碎裂許多，基本上不能打硬球QQ....只能打練習場的軟球了。想約打棒球的也可以約起來ʘ‿ʘ",
+         date:"留言日期：2023 / 07 / 06"},
+        {name:"棒球迷阿真",
+         textAll:"你好我有興趣！！",
+         date:"留言日期：2023 / 07 / 06"},
+      ],
+      noticeText:{
+        Title:"請注意：",
+        Text:"平台僅供會員自由張貼拍賣訊息。進行交易時，確實比對貨品規格是否符合，並留下對方真實姓名(最好記下身分證字號)、聯絡方式，務必確認完整再行付款，以免產生糾紛。建議用網站匯款方式進行交易，較有保障。",
+      }
     };
   },
 };
@@ -230,6 +295,132 @@ export default {
     & button:hover {
       background-color: var(--error-red);
     }
+  }
+  //留言板
+  .product_message {
+    .message_title{
+      width: 96.7%;
+      display: flex;
+      gap: 1.5rem;
+      padding: 2rem 0rem 1.25rem 0rem;
+      margin: 2rem 0 0 2rem;
+      border-bottom: 2px solid var(--secondary-gray-2);
+      font-weight: 500;
+      letter-spacing: 6px;
+      font-size: 1.25rem;
+      & div {
+        display: inline-block;
+      }
+    }
+    .message_area {
+      width: 100%;
+      .message_all {
+        display: flex;
+        padding: 3rem 0 3rem 0;
+        margin-left: 2rem;
+        border-bottom: 2px solid var(--secondary-gray-2);
+        .user_pic {
+          width: 12%;
+          position: relative;
+          .user {
+          width: 100%;
+          }
+          .mark {
+            position: absolute;
+            top: -25px;
+            left: 5px;
+          }
+        }
+        .message_text {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          margin-left: 2rem;
+          .message_name {
+            font-size: 1.2rem;
+          }
+          .message_text_all{
+            margin-top: 1rem;
+            color: var(--secondary-gray-1);
+          }
+          .message_date {
+            margin-top: 2rem;
+            text-align: right;
+            color: var(--secondary-gray-1);
+            & img {
+              margin-right: 1%;
+            }
+        }
+        }
+      }
+    }
+    .message_importing {
+      width: 100%;
+      display: flex;
+      padding: 3rem 0 3rem 2rem;
+      .importing_text {
+        width: 100%;
+        & textarea {
+          display: block;
+          width: 90%;
+          min-height: 200px;
+          margin-left: 2rem;
+          font-size: 1rem;
+          padding: 1rem;
+          background-color: var(--secondary-blue-4);
+          border-radius: var(--round);
+          border: none;
+          &::placeholder {
+            color: var(--secondary-gray-3);
+          }
+        }
+        & input {
+          display: block;
+          margin-left: auto;
+          margin-right: 3rem;
+          margin-top: 2rem;
+          width: 150px;
+          height: 50px;
+          background-color: var(--primary-blue);
+          color: #fff;
+          font-size: 1.25rem;
+          font-weight: 500;
+          letter-spacing: 6px;
+          border-radius: var(--round);
+          border: none;
+        }
+      }
+    }
+    .notice {
+      width: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      background-color: var(--secondary-blue-3);
+      border-radius: var(--round);
+      padding: 3rem 1rem;
+      margin-bottom: 4rem;
+      .notice_icon {
+        width: 5%;
+        & img {
+          width: 100%;
+        }
+      }
+      .notice_text {
+        width: 85%;
+        background-color: #fff;
+        padding: 2rem;
+        border-radius: var(--round);
+        .title {
+          font-size: 1.25rem;
+          color: var(--error-red);
+        }
+        .text {
+          margin-top: 1rem;
+          color: var(--secondary-gray-1);
+        }
+      }
+    }
+
   }
 }
 </style>
