e722036cef5c8129a4788f4085e9847bdbcf48ef
Kennedy1127 <zxc1258741@gmail.com>
2023-07-12 18:13:34 +0800

    解衝突

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/HomeView.vue'),
+    path: "/",
+    name: "home",
+    component: () => import("@/views/HomeView.vue"),
   },
   {
-    path: '/products',
-    name: 'products',
-    component: () => import('@/views/ProductsView.vue'),
+    path: "/products",
+    name: "products",
+    component: () => import("@/views/ProductsView.vue"),
   },
   {
-<<<<<<< HEAD
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/LoginView.vue'),
-=======
-    path: '/productsdetail',
-    name: 'productsdetail',
-    component: () => import('@/views/ProductDetails.vue'),
->>>>>>> db83cc1c6a3193fc03a76ec82fe5381e26426a21
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+    path: "/productsdetail",
+    name: "productsdetail",
+    component: () => import("@/views/ProductDetails.vue"),
   },
 ];
 
