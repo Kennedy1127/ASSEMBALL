441591bce27dc0764d41a124b0f5bfff9ad2193e
zoey03yang <zxcsuki@gmail.com>
2023-07-13 16:29:02 +0800

    結構和class名稱修改

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -23,37 +23,45 @@
         </aside>
 
         <main class="products_main">
-          <section class="product_detail">
-            <div class="detail_pic">
+          <section class="product_main_detail">
+            <div class="product_main_detail_pic">
               <img
                 src="~@/assets/images/products/productdetail_pic1.png"
                 alt="productdetail_pic1"
               />
-              <div class="smallpic">
-                <img
-                  src="~@/assets/images/products/productdetail_smallpic1.png"
-                  alt="productdetail_pic1"
-                />
-                <img
-                  src="~@/assets/images/products/productdetail_smallpic1.png"
-                  alt="productdetail_pic1"
-                />
-                <img
-                  src="~@/assets/images/products/productdetail_smallpic1.png"
-                  alt="productdetail_pic1"
-                />
-                <img
-                  src="~@/assets/images/products/productdetail_smallpic1.png"
-                  alt="productdetail_pic1"
-                />
+              <div class="product_main_detail_pic_all">
+                <div class="product_main_detail_pic_all_small">
+                  <img
+                    src="~@/assets/images/products/productdetail_smallpic1.png"
+                    alt="productdetail_pic1"
+                  />
+                </div>
+                <div class="product_main_detail_pic_all_small">
+                  <img
+                    src="~@/assets/images/products/productdetail_smallpic1.png"
+                    alt="productdetail_pic1"
+                  />
+                </div>
+                <div class="product_main_detail_pic_all_small">
+                  <img
+                    src="~@/assets/images/products/productdetail_smallpic1.png"
+                    alt="productdetail_pic1"
+                  />
+                </div>
+                <div class="product_main_detail_pic_all_small">
+                  <img
+                    src="~@/assets/images/products/productdetail_smallpic1.png"
+                    alt="productdetail_pic1"
+                  />
+                </div>
               </div>
             </div>
-            <div class="detail_content">
-              <div class="detail_title">
+            <div class="product_main_detail_content">
+              <div class="product_main_detail_content_title">
                 <span>{{ productDetail.Title }}</span>
               </div>
               <div
-                class="detail_body"
+                class="product_main_detail_content_body"
                 v-for="item in productDetailBody"
                 :key="item"
               >
@@ -66,52 +74,73 @@
             </div>
           </section>
           <section class="product_message">
-            <div class="message_title">
-              <div class="block"></div>
-              <div>{{ productMessage.Title }}</div>
+            <div class="product_message_title">
+              <div class="product_message_title_block"></div>
+              <span>{{ productMessage.Title }}</span>
             </div>
-            <div class="message_area">
-              <div class="message_all" v-for="item in messageArea" :key="item">
-                <div class="user_pic">
-                  <img class="mark" src="~@/assets/images/products/mark.png"
-                    alt="mark"/> 
-                  <img class="user" src="~@/assets/images/products/message_pic01.png"
-                  alt="message_pic01"/>  
+            <div class="product_message_area">
+              <div
+                class="product_message_area_all"
+                v-for="item in messageArea"
+                :key="item"
+              >
+                <div class="product_message_area_all_pic">
+                  <img
+                    class="product_message_area_all_pic_mark"
+                    src="~@/assets/images/products/mark.png"
+                    alt="mark"
+                  />
+                  <img
+                    class="product_message_area_all_pic_user"
+                    src="~@/assets/images/products/message_pic01.png"
+                    alt="message_pic01"
+                  />
                 </div>
-                <div class="message_text">
-                  <div class="message_name">{{ item.name }}</div>
-                  <div class="message_text_all">{{ item.textAll }}</div>
-                  <div class="message_date">
-                    <img
-                    src="~@/assets/images/products/edit.png"
-                    alt="edit"
-                    />
-                    {{ item.date }}</div>
+                <div class="product_message_area_all_text">
+                  <div class="product_message_area_all_text_name">
+                    {{ item.name }}
+                  </div>
+                  <div class="product_message_area_all_text_all">
+                    {{ item.textAll }}
+                  </div>
+                  <div class="product_message_area_all_text_date">
+                    <img src="~@/assets/images/products/edit.png" alt="edit" />
+                    {{ item.date }}
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="message_importing">
-              <div class="user_pic">
-                  <img
+            <div class="product_message_importing">
+              <div class="product_message_importing_user_pic">
+                <img
                   src="~@/assets/images/products/importing_pic.png"
                   alt="importing_pic"
                 />
               </div>
-              <div class="importing_text">
-                <textarea placeholder="請輸入留言內容......" name="comment" form="usrform"></textarea>
-                <input type="submit" value="送出" />
+              <div class="product_message_importing_text">
+                <textarea
+                  placeholder="請輸入留言內容......"
+                  name="comment"
+                  form="usrform"
+                ></textarea>
+                <button>送出</button>
               </div>
             </div>
-            <div class="notice">
-              <div class="notice_icon">
-                <img
-                  src="~@/assets/images/products/notice.png"
-                  alt="notice"
-                />
+            <div class="product_message_notice">
+              <div class="product_message_notice_icon">
+                <img src="~@/assets/images/products/notice.png" alt="notice" />
               </div>
-              <div class="notice_text">
-                <div class="title">{{ noticeText.Title }}</div>
-                <div class="text">{{ noticeText.Text }}</div>
+              <div class="product_message_notice_text">
+                <div class="product_message_notice_text_title">
+                  {{ noticeText.Title }}
+                </div>
+                <div class="product_message_notice_text_detail">
+                  {{ noticeText.Text }}
+                  <br />
+                  {{ noticeText.TextTwo }}
+                  <br />
+                  {{ noticeText.TextThree }}
+                </div>
               </div>
             </div>
           </section>
@@ -154,21 +183,29 @@ export default {
         { title: "Email : ", text: "hawhaw052@gmail.com" },
         { title: "Phone : ", text: "0953299481" },
       ],
-      productMessage:{
-        Title:"留言板" ,
+      productMessage: {
+        Title: "留言板",
       },
       messageArea: [
-        {name:"棒球專家小楊",
-         textAll:"小弟這隻是Louisville slugger木棒，打了大約四年，有些地方碎裂許多，基本上不能打硬球QQ....只能打練習場的軟球了。想約打棒球的也可以約起來ʘ‿ʘ",
-         date:"留言日期：2023 / 07 / 06"},
-        {name:"棒球迷阿真",
-         textAll:"你好我有興趣！！",
-         date:"留言日期：2023 / 07 / 06"},
+        {
+          name: "棒球專家小楊",
+          textAll:
+            "小弟這隻是Louisville slugger木棒，打了大約四年，有些地方碎裂許多，基本上不能打硬球QQ....只能打練習場的軟球了。想約打棒球的也可以約起來ʘ‿ʘ",
+          date: "留言日期：2023 / 07 / 06",
+        },
+        {
+          name: "棒球迷阿真",
+          textAll: "你好我有興趣！！",
+          date: "留言日期：2023 / 07 / 06",
+        },
       ],
-      noticeText:{
-        Title:"請注意：",
-        Text:"平台僅供會員自由張貼拍賣訊息。進行交易時，確實比對貨品規格是否符合，並留下對方真實姓名(最好記下身分證字號)、聯絡方式，務必確認完整再行付款，以免產生糾紛。建議用網站匯款方式進行交易，較有保障。",
-      }
+      noticeText: {
+        Title: "請注意：",
+        Text: "平台僅供會員自由張貼拍賣訊息。",
+        TextTwo:
+          "進行交易時，確實比對貨品規格是否符合，並留下對方真實姓名(最好記下身分證字號)、聯絡方式，務必確認完整再行付款，以免產生糾紛。",
+        TextThree: "建議用網站匯款方式進行交易，較有保障。",
+      },
     };
   },
 };
@@ -235,136 +272,156 @@ export default {
   }
 
   //商品詳情
-  .product_detail {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    .detail_pic {
+  .product_main {
+    &_detail {
+      width: 100%;
       display: flex;
-      flex-direction: column;
-      padding-left: 2rem;
-      .smallpic {
+      flex-direction: row;
+      justify-content: center;
+      &_pic {
+        width: 60%;
         display: flex;
-        padding-top: 1rem;
-        gap: 0.75rem;
+        flex-direction: column;
+        padding-left: 2rem;
+        &_all {
+          width: 100%;
+          display: flex;
+          padding-top: 1rem;
+          gap: 0.75rem;
+          &_small {
+            width: 25%;
+            & img {
+              width: 100%;
+            }
+          }
+        }
+      }
+      &_content {
+        width: 80%;
+        margin-left: 2rem;
+        &_title {
+          padding-bottom: 2rem;
+          & span {
+            color: var(--primary-black);
+            font-size: 2rem;
+            font-weight: 500;
+            letter-spacing: 1.5px;
+          }
+        }
+        &_body {
+          color: var(--secondary-gray-1);
+          padding: 0.75rem 0.75rem 0.75rem 0;
+          border-bottom: 1px solid var(--secondary-gray-3);
+          letter-spacing: 1.5px;
+        }
+      }
+      .detail_footer {
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+        .detail_price {
+          font-weight: 500;
+          letter-spacing: 1.5px;
+          font-size: 1.75rem;
+          color: var(--error-red);
+          padding-top: 3rem;
+        }
+        & button {
+          width: 150px;
+          height: 50px;
+          font-size: 1.25rem;
+          font-weight: 500;
+          letter-spacing: 6px;
+          border-radius: var(--round);
+          color: #ffffff;
+          background-color: var(--error-yellow);
+          transition: all 0.2s ease-in-out;
+        }
+        & button:hover {
+          background-color: var(--error-red);
+        }
       }
     }
   }
-  .detail_content {
-    width: 80%;
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: space-between;
-    margin-left: 4rem;
-    .detail_title {
-      padding-bottom: 2.5rem;
-      & span {
-        color: var(--primary-black);
-        font-size: 2rem;
-        font-weight: 500;
-        letter-spacing: 1.5px;
-      }
-    }
-    .detail_body {
-      color: var(--secondary-gray-1);
-      padding: 0.75rem 0.75rem 0.75rem 0;
-      border-bottom: 1px solid var(--secondary-gray-3);
-      letter-spacing: 1.5px;
-    }
-  }
-  .detail_footer {
-    display: flex;
-    align-items: flex-end;
-    justify-content: space-between;
-    .detail_price {
-      font-weight: 500;
-      letter-spacing: 1.5px;
-      font-size: 2rem;
-      color: var(--error-red);
-      padding-top: 4rem;
-    }
-    & button {
-      font-size: 1.25rem;
-      font-weight: 600;
-      padding: 0.75rem 3rem;
-      border-radius: var(--round);
-      color: #ffffff;
-      background-color: var(--error-yellow);
-      transition: all 0.2s ease-in-out;
-    }
-    & button:hover {
-      background-color: var(--error-red);
-    }
-  }
+
   //留言板
   .product_message {
-    .message_title{
-      width: 96.7%;
+    padding: 2rem;
+    padding-right: 0;
+    &_title {
+      width: 100%;
       display: flex;
       gap: 1.5rem;
-      padding: 2rem 0rem 1.25rem 0rem;
-      margin: 2rem 0 0 2rem;
+      padding: 2rem 0 2rem 0;
       border-bottom: 2px solid var(--secondary-gray-2);
       font-weight: 500;
       letter-spacing: 6px;
       font-size: 1.25rem;
-      & div {
-        display: inline-block;
+      &_block {
+        width: 8px;
+        height: 26px;
+        background-color: var(--primary-blue);
       }
     }
-    .message_area {
+    &_area {
       width: 100%;
-      .message_all {
+      &_all {
         display: flex;
-        padding: 3rem 0 3rem 0;
-        margin-left: 2rem;
+        padding: 4rem 0 1rem 0;
         border-bottom: 2px solid var(--secondary-gray-2);
-        .user_pic {
-          width: 12%;
+        &_pic {
+          width: 6rem;
           position: relative;
-          .user {
-          width: 100%;
+          &_user {
+            width: 100%;
           }
-          .mark {
+          &_mark {
             position: absolute;
-            top: -25px;
-            left: 5px;
+            top: -26px;
+            left: 0px;
           }
         }
-        .message_text {
+        &_text {
           width: 100%;
           display: flex;
           flex-direction: column;
           margin-left: 2rem;
-          .message_name {
-            font-size: 1.2rem;
+          &_name {
+            font-size: 1.25rem;
           }
-          .message_text_all{
+          &_all {
             margin-top: 1rem;
             color: var(--secondary-gray-1);
           }
-          .message_date {
-            margin-top: 2rem;
+          &_date {
+            margin-top: 4rem;
             text-align: right;
             color: var(--secondary-gray-1);
+            font-size: 0.875rem;
             & img {
-              margin-right: 1%;
+              margin-right: 1rem;
             }
-        }
+          }
         }
       }
     }
-    .message_importing {
+    &_importing {
       width: 100%;
       display: flex;
-      padding: 3rem 0 3rem 2rem;
-      .importing_text {
+      padding: 4rem 0 4rem 0;
+      &_user_pic {
+        width: 6rem;
+        & img {
+          width: 100%;
+        }
+      }
+      &_text {
         width: 100%;
         & textarea {
-          display: block;
           width: 90%;
-          min-height: 200px;
-          margin-left: 2rem;
+          display: block;
+          margin: 0 2rem 0 2rem;
+          min-height: 192px;
           font-size: 1rem;
           padding: 1rem;
           background-color: var(--secondary-blue-4);
@@ -374,53 +431,52 @@ export default {
             color: var(--secondary-gray-3);
           }
         }
-        & input {
-          display: block;
-          margin-left: auto;
-          margin-right: 3rem;
-          margin-top: 2rem;
-          width: 150px;
-          height: 50px;
-          background-color: var(--primary-blue);
-          color: #fff;
-          font-size: 1.25rem;
-          font-weight: 500;
-          letter-spacing: 6px;
-          border-radius: var(--round);
-          border: none;
-        }
+      }
+      & button {
+        width: 150px;
+        height: 50px;
+        display: block;
+        background-color: var(--primary-blue);
+        color: #fff;
+        font-size: 1.25rem;
+        font-weight: 500;
+        letter-spacing: 6px;
+        border-radius: var(--round);
+        border: none;
+        margin-left: auto;
+        margin-top: 2rem;
+        margin-right: 2rem;
       }
     }
-    .notice {
+    &_notice {
       width: 100%;
       display: flex;
       justify-content: space-evenly;
       background-color: var(--secondary-blue-3);
       border-radius: var(--round);
       padding: 3rem 1rem;
-      margin-bottom: 4rem;
-      .notice_icon {
+      margin-bottom: 2rem;
+      &_icon {
         width: 5%;
         & img {
           width: 100%;
         }
       }
-      .notice_text {
+      &_text {
         width: 85%;
         background-color: #fff;
         padding: 2rem;
         border-radius: var(--round);
-        .title {
+        &_title {
           font-size: 1.25rem;
           color: var(--error-red);
         }
-        .text {
+        &_detail {
           margin-top: 1rem;
           color: var(--secondary-gray-1);
         }
       }
     }
-
   }
 }
 </style>
