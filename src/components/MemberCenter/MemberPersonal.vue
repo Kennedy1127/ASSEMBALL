8632059050b1d79d7afe872766c72bcf43d70c6f
shaash <lynn123123999@gmail.com>
2023-07-26 11:09:15 +0800

    應徵模板驗證 + 會員中心 新增 登入&註冊按鈕

--- src/components/MemberCenter/MemberPersonal.vue
+++ src/components/MemberCenter/MemberPersonal.vue
@@ -427,6 +427,62 @@ export default {
       display: flex;
       flex-direction: column;
       padding-bottom: 0.5rem;
+      .ivu-select-single {
+        height: 100%;
+
+        .ivu-select-selection {
+          height: 100%;
+          border: 3px solid var(--secondary-blue-2);
+          border-radius: 10px;
+          color: var(--secondary-blue-2);
+
+          .ivu-select-placeholder {
+            color: var(--secondary-blue-2);
+          }
+
+          div:first-of-type {
+            height: 100%;
+            padding: 1rem 1.5rem;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            span {
+              height: 100%;
+              padding: 0;
+
+              display: flex;
+              align-items: center;
+
+              font-family: "Noto Sans TC", "Montserrat", sans-serif;
+              font-size: 1.25rem;
+              font-weight: 400;
+            }
+
+            i {
+              position: relative;
+              top: initial;
+              right: initial;
+              transform: translateY(0);
+              width: 24px;
+              height: 24px;
+
+              &::before {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -40%);
+                font-size: 2rem;
+                color: var(--secondary-blue-2);
+              }
+            }
+          }
+        }
+      }
+
+      .ivu-select-visible .ivu-select-arrow {
+        transform: rotate(180deg) !important;
+      }
       & label {
         font-size: 1rem;
         color: var(--secondary-gray-1);
