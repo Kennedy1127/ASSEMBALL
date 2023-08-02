a5058ddbcd16f9367d2dab331c383a1662ef838a
Kennedy1127 <zxc1258741@gmail.com>
2023-08-02 10:03:27 +0800

    手機版登入功能完成

--- src/views/authentications/LogInView.vue
+++ src/views/authentications/LogInView.vue
@@ -73,7 +73,11 @@
     <AuthenticationPic :info="info" />
   </AuthenticationWrapper>
 
-  <LoginMobile v-if="store.state.isMobile" />
+  <LoginMobile
+    v-if="store.state.isMobile"
+    :error="error"
+    @mobileSignin="handleMobile"
+  />
 </template>
 
 <script setup>
@@ -82,6 +86,7 @@ import AuthenticationPic from "@/components/Authentication/AuthenticationPic.vue
 import LoginMobile from "@/components/Authentication/mobile/LoginMobile.vue";
 import useSignin from "@/composables/authentication/useSignin";
 import useSetPersistence from "@/composables/authentication/useSetPersistence";
+import getData from "@/composables/data/getData";
 import { ref } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
@@ -90,6 +95,7 @@ const store = useStore();
 const router = useRouter();
 const { signinError, signin } = useSignin();
 const { changePersistence } = useSetPersistence();
+const { getUser } = getData();
 
 const info = {
   title: "Hello , Friend !",
@@ -124,6 +130,13 @@ const checkFormat = () => {
   if (!password.value) return (error.value = "請輸入你的登入密碼");
 };
 
+const handleMobile = (signinData) => {
+  email.value = signinData.email;
+  password.value = signinData.password;
+
+  handleSignin();
+};
+
 const handleSignin = async () => {
   store.state.isPending = true;
 
@@ -143,9 +156,11 @@ const handleSignin = async () => {
     return (store.state.isPending = false);
   }
 
+  store.state.user = await getUser();
+  store.state.isLoggedIn = true;
+
   router.push({ name: "Home" });
   store.state.isPending = false;
-  store.state.isLoggedIn = true;
 };
 </script>
 
